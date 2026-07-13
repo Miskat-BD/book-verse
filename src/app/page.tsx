95f2dad450ce352703b064848400058c46e5ff2b
Miskat-BD <mishkatahmed321@gmail.comgit config --global user.name Miskat-BDgit commit -m first>
2026-07-13 17:01:12 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@heroui/react";
+
+export default function HomePage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-4xl font-bold">Welcome to My App</h1>
+      <Button>Click Me</Button>
+    </main>
+  );
+}
